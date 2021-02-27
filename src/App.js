5864cc97f6936f21db1360717b46f74eaf678b52
DiegoMutre <tysonmutre10@gmail.com>
2021-02-27 16:54:50 -0500

    feat: Create routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import EditProduct from "./components/EditProduct";
 import Header from "./components/Header";
+import NewProduct from "./components/NewProduct";
+import Products from "./components/Products";
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-        </>
+            <div className="container">
+                <Switch>
+                    <Route exact path="/" component={Products} />
+                    <Route exact path="/products/new" component={NewProduct} />
+                    <Route
+                        exact
+                        path="/products/edit/:id"
+                        component={EditProduct}
+                    />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
