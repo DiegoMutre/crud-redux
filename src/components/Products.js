25593762eebdd27fe5624d2c1a03eb8024336a7f
DiegoMutre <tysonmutre10@gmail.com>
2021-02-27 17:15:53 -0500

    feat: Create Products.js jsx structure.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,19 @@
 const Products = () => {
-    return <h1>From Products.js</h1>;
+    return (
+        <>
+            <h2 className="text-center my-5">Products List</h2>
+            <table className="table table-stripped">
+                <thead className="bg-primary table-dark">
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>{/* Show data from the API */}</tbody>
+            </table>
+        </>
+    );
 };
 
 export default Products;
