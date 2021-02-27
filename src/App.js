d01dd7bdf60c1b377f7f025e6868b31d54b8edb9
DiegoMutre <tysonmutre10@gmail.com>
2021-02-27 17:59:14 -0500

    feat: Use Provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import EditProduct from "./components/EditProduct";
 import Header from "./components/Header";
 import NewProduct from "./components/NewProduct";
 import Products from "./components/Products";
+import store from "./store";
+
+// Redux
 
 function App() {
     return (
         <BrowserRouter>
-            <Header />
-            <div className="container mt-5">
-                <Switch>
-                    <Route exact path="/" component={Products} />
-                    <Route exact path="/products/new" component={NewProduct} />
-                    <Route
-                        exact
-                        path="/products/edit/:id"
-                        component={EditProduct}
-                    />
-                </Switch>
-            </div>
+            <Provider store={store}>
+                <Header />
+                <div className="container mt-5">
+                    <Switch>
+                        <Route exact path="/" component={Products} />
+                        <Route
+                            exact
+                            path="/products/new"
+                            component={NewProduct}
+                        />
+                        <Route
+                            exact
+                            path="/products/edit/:id"
+                            component={EditProduct}
+                        />
+                    </Switch>
+                </div>
+            </Provider>
         </BrowserRouter>
     );
 }
