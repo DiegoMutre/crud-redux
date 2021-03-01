62291a7180d670fcbf8602714f16687ccbca8aa9
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 07:21:39 -0500

    feat: Use getProductsAction in Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductsAction } from "../actions/productsActions";
+
 const Products = () => {
+    const dispatch = useDispatch();
+    const loading = useSelector(state => state.products.loading);
+
+    useEffect(() => {
+        dispatch(getProductsAction());
+        console.log("LOOP");
+    }, [dispatch]);
+
     return (
         <>
             <h2 className="text-center my-5">Products List</h2>
+            {loading && <p className="text-center">Loading...</p>}
             <table className="table table-stripped">
                 <thead className="bg-primary table-dark">
                     <tr>
