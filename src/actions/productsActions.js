9a52982841e11a3b133358cdbba453c352bba20c
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:22:52 -0500

    feat: Insert product in the fake db with axios.

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -1,13 +1,27 @@
-import { ADD_PRODUCT, ADD_PRODUCT_SUCCESSFUL } from "../types";
+import { axiosClient } from "../config/axios";
+import {
+    ADD_PRODUCT,
+    ADD_PRODUCT_ERROR,
+    ADD_PRODUCT_SUCCESSFUL,
+} from "../types";
 
 // Create new products
-export const createNewProductAction = product => dispatch => {
+export const createNewProductAction = product => async dispatch => {
     dispatch({
         type: ADD_PRODUCT,
         payload: true,
     });
-    dispatch({
-        type: ADD_PRODUCT_SUCCESSFUL,
-        payload: product,
-    });
+
+    try {
+        await axiosClient.post("/", product);
+        dispatch({
+            type: ADD_PRODUCT_SUCCESSFUL,
+            payload: product,
+        });
+    } catch (error) {
+        dispatch({
+            type: ADD_PRODUCT_ERROR,
+            payload: true,
+        });
+    }
 };
