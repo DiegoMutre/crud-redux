befe555fa26f3784f5c417132fb91bedefd12c88
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 18:06:21 -0500

    feat: Create func to load the product to edit in EditProduct.js

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 const EditProduct = () => {
+    const [product, setProduct] = useState({
+        name: "",
+        price: 0,
+    });
+    const productToEdit = useSelector(state => state.products.productToEdit);
+
+    useEffect(() => {
+        if (productToEdit) {
+            setProduct(productToEdit);
+        }
+    }, [productToEdit]);
+
+    const handleChange = e => {
+        setProduct({
+            ...product,
+            [e.target.name]: isNaN(+e.target.value)
+                ? e.target.value
+                : +e.target.value,
+        });
+    };
+
     return (
         <div className="row justify-content-center">
             <div className="col-md-8">
@@ -15,6 +39,8 @@ const EditProduct = () => {
                                     className="form-control"
                                     placeholder="Product Name"
                                     name="name"
+                                    onChange={handleChange}
+                                    value={product.name}
                                 />
                             </div>
                             <div className="form-group">
@@ -24,6 +50,8 @@ const EditProduct = () => {
                                     className="form-control"
                                     placeholder="Product Name"
                                     name="price"
+                                    onChange={handleChange}
+                                    value={+product.price}
                                 />
                             </div>
                             <button
