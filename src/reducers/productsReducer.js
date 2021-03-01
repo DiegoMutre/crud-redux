cd57c8083b1000ac540d90a4061beec6a5039d70
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:07:47 -0500

    feat: Create reducers in productsReducer.js

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,3 +1,5 @@
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESSFUL } from "../types";
+
 // Each reducer have their own state
 const initialState = {
     products: [],
@@ -7,6 +9,16 @@ const initialState = {
 
 function productsReducer(state = initialState, action) {
     switch (action.type) {
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                loading: action.payload,
+            };
+        case ADD_PRODUCT_SUCCESSFUL:
+            return {
+                ...state,
+                products: [...state.products, action.payload],
+            };
         default:
             return state;
     }
