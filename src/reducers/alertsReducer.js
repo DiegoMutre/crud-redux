3f3e22356d6261ad221f2f085e7dfc43dfa90c72
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:40:48 -0500

    feat: Create new action type in alertsReducer.js

--- src/reducers/alertsReducer.js
+++ src/reducers/alertsReducer.js
@@ -1,9 +1,16 @@
+import { SHOW_ALERT } from "../types";
+
 const initialState = {
     alert: null,
 };
 
 const alertsReducer = (state = initialState, action) => {
     switch (action.type) {
+        case SHOW_ALERT:
+            return {
+                ...state,
+                alert: action.payload,
+            };
         default:
             return state;
     }
