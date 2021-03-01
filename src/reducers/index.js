dd799dcc25c573aae33d0bfa1f231d71f7d754ce
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:29:39 -0500

    feat: Create alertsReducer.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import alertsReducer from "./alertsReducer";
 import productsReducer from "./productsReducer";
 
 export default combineReducers({
     products: productsReducer,
+    alert: alertsReducer,
 });
