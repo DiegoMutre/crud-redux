940904f8ea859765691a010e649877ad14879f7d
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:53:45 -0500

    feat: Create new action in alertActions.js

--- src/actions/alertActions.js
+++ src/actions/alertActions.js
@@ -1,5 +1,6 @@
-import { SHOW_ALERT } from "../types";
+import { HIDE_ALERT, SHOW_ALERT } from "../types";
 
-export const showAlertAction = alert => dispatch => {
+export const showAlertAction = alert => dispatch =>
     dispatch({ type: SHOW_ALERT, payload: alert });
-};
+
+export const hideAlertAction = () => dispatch => dispatch({ type: HIDE_ALERT });
