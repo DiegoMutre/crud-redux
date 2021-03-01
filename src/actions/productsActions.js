b0478174a16de8ec80d90230cd539f7d54673f90
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:08:24 -0500

    feat: Create one action in productsActions.js

--- src/actions/productsActions.js
+++ src/actions/productsActions.js
@@ -1,6 +1,13 @@
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESSFUL } from "../types";
+
 // Create new products
-export const createNewProductAction = product => {
-    return () => {
-        console.log(product);
-    };
+export const createNewProductAction = product => dispatch => {
+    dispatch({
+        type: ADD_PRODUCT,
+        payload: true,
+    });
+    dispatch({
+        type: ADD_PRODUCT_SUCCESSFUL,
+        payload: product,
+    });
 };
