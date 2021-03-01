43bb4408093aba3dfe2c6cf1791d12dd7c6561a8
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 08:45:35 -0500

    feat: Delete a product through a button.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,28 @@
 import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
+import { deleteProductAction } from "../actions/productsActions";
 
 const Product = ({ product: { id, name, price } }) => {
+    const dispatch = useDispatch();
+
+    const handleDelete = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "A product deleted can't be recovered",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete",
+            cancelButtonText: "Cancel",
+        }).then(result => {
+            if (result.value) {
+                dispatch(deleteProductAction(id));
+            }
+        });
+    };
+
     return (
         <tr>
             <td>{name}</td>
@@ -9,7 +31,9 @@ const Product = ({ product: { id, name, price } }) => {
             </td>
             <td className="acciones">
                 <button className="btn btn-primary mr-2">Edit</button>
-                <button className="btn btn-danger">Delete</button>
+                <button className="btn btn-danger" onClick={handleDelete}>
+                    Delete
+                </button>
             </td>
         </tr>
     );
