a640614ffa1cfdcf55a68ed91cf4dc0b79690a45
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 06:30:53 -0500

    feat: Create new types

--- src/types/index.js
+++ src/types/index.js
@@ -1,3 +1,6 @@
 export const ADD_PRODUCT = "ADD_PRODUCT";
 export const ADD_PRODUCT_SUCCESSFUL = "ADD_PRODUCT_SUCCESSFUL";
 export const ADD_PRODUCT_ERROR = "ADD_PRODUCT_ERROR";
+
+export const SHOW_ALERT = "SHOW_ALERT";
+export const HIDE_ALERT = "HIDE_ALERT";
