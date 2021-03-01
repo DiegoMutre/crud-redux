b72e505ca29ea65cc33033effcafc2c64491cfa2
DiegoMutre <tysonmutre10@gmail.com>
2021-03-01 07:38:05 -0500

    feat: Use Product.js in Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductsAction } from "../actions/productsActions";
+import Product from "./Product";
 
 const Products = () => {
     const dispatch = useDispatch();
     const loading = useSelector(state => state.products.loading);
+    const products = useSelector(state => state.products.products);
+    const error = useSelector(state => state.products.error);
 
     useEffect(() => {
         dispatch(getProductsAction());
@@ -14,6 +17,11 @@ const Products = () => {
     return (
         <>
             <h2 className="text-center my-5">Products List</h2>
+            {error && (
+                <p className="font-weight-bold alert alert-danger text-center mt-4">
+                    There was a mistake, reload the page
+                </p>
+            )}
             {loading && <p className="text-center">Loading...</p>}
             <table className="table table-stripped">
                 <thead className="bg-primary table-dark">
@@ -23,7 +31,11 @@ const Products = () => {
                         <th scope="col">Actions</th>
                     </tr>
                 </thead>
-                <tbody>{/* Show data from the API */}</tbody>
+                <tbody>
+                    {products.map(product => (
+                        <Product key={product.id} product={product} />
+                    ))}
+                </tbody>
             </table>
         </>
     );
